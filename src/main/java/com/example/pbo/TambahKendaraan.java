67df4f97cd5390b51a899e7b5fc7661b4e520caf
farhangold <121116261+farhangold@users.noreply.github.com>
2023-05-24 18:12:06 +0700

    Tambah Kendaraan

---
+++ src/main/java/com/example/pbo/TambahKendaraan.java
@@ -0,0 +1,19 @@
+package com.example.pbo;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+
+public class TambahKendaraan {
+    @FXML
+    public TextField DateTime;
+    @FXML
+    public TextField TambahPlatNomer;
+    @FXML
+    public DatePicker DateTimePicker;
+    @FXML
+    public TextField JenisKendaraan;
+    @FXML
+    public Button btnTambah;
+}
